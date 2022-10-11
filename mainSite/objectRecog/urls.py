00a42fa2afc00d8717344713f590b5c56daaaa0f
unknown <lsj8249@naver.com>
2022-10-12 00:30:36 +0900

    gitignore 적용

--- mainSite/objectRecog/urls.py
+++ mainSite/objectRecog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.objectIndex, name='objectIndex'),
+  path('', views.objectIndex, name='objectIndex'),
+  path('kakaoApi/', views.kakaoApi, name="kakaoApi"),
 ]
--- objectRecog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.objectIndex, name='objectIndex'),
-    path('kakaoApi/', views.kakaoApi, name="kakaoApi"),
-]
